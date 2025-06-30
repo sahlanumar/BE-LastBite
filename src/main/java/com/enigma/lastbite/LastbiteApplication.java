2e747b4ed93adc7b084e53850ed0cb676c9c1c4f
Mohamad Sahlan Umar <msahlanumar@gmail.com>
2025-06-30 09:28:43 +0700

    add skeleton

---
+++ src/main/java/com/enigma/lastbite/LastbiteApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.lastbite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastbiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastbiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enigma/lastbite/LastbiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.lastbite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastbiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
