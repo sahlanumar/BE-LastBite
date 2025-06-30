de73c4fed4b58b2ecb8c3c96aec9a3295dc3d852
Mohamad Sahlan Umar <msahlanumar@gmail.com>
2025-06-30 09:46:12 +0700

    add Common response

---
+++ src/main/java/com/enigma/lastbite/dto/response/CommonResponse.java
@@ -0,0 +1,22 @@
+package com.enigma.lastbite.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CommonResponse<T> {
+    private Integer statusCode;
+    private String message;
+    private T data;
+    private PagingResponse paging;
+    
+    @Builder.Default
+    private LocalDateTime timestamp = LocalDateTime.now();
+}
