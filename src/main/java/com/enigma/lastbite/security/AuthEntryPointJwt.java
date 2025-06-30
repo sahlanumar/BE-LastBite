90c0ca20a572e9ce23f5a3fcd1f870d99b5f81b0
Mohamad Sahlan Umar <msahlanumar@gmail.com>
2025-06-30 10:13:13 +0700

    add security

---
+++ src/main/java/com/enigma/lastbite/security/AuthEntryPointJwt.java
@@ -0,0 +1,43 @@
+package com.enigma.lastbite.security;
+
+
+import com.enigma.lastbite.dto.response.CommonResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@Component
+@Slf4j
+public class AuthEntryPointJwt implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        log.error("Unauthorized error: {}", authException.getMessage());
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        CommonResponse<Object> errorResponse = CommonResponse.builder()
+                .statusCode(HttpStatus.UNAUTHORIZED.value())
+                .message("Unauthorized: " + authException.getMessage())
+                .data(null)
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        final ObjectMapper mapper = new ObjectMapper();
+        // Configure object mapper for LocalDateTime
+        mapper.findAndRegisterModules();
+        mapper.writeValue(response.getOutputStream(), errorResponse);
+    }
+}
