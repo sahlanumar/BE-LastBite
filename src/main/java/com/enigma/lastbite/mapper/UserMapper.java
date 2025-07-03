b2f8d3d41f2fc3cedc188604b78ff63776d2d395
Mohamad Sahlan Umar <msahlanumar@gmail.com>
2025-07-03 09:21:52 +0700

    Feat sahlan : Refactor kode (using mapper on seller and user)

--- src/main/java/com/enigma/lastbite/mapper/UserMapper.java
+++ src/main/java/com/enigma/lastbite/mapper/UserMapper.java
@@ -1,12 +1,29 @@
 package com.enigma.lastbite.mapper;
 
+import com.enigma.lastbite.dto.request.UpdateUserRequest;
 import com.enigma.lastbite.dto.response.UserResponse;
 import com.enigma.lastbite.entity.User;
 
 public class UserMapper {
+
+    public static void updateFromDto(User user, UpdateUserRequest request) {
+        if (request.getFullName() != null) {
+            user.setFullName(request.getFullName());
+        }
+        if (request.getEmail() != null) {
+            user.setEmail(request.getEmail());
+        }
+        if (request.getPhoneNumber() != null) {
+            user.setPhoneNumber(request.getPhoneNumber());
+        }
+        if (request.getSuspendedUntil() != null) {
+            user.setSuspendedUntil(request.getSuspendedUntil());
+        }
+    }
+
     public static UserResponse toUserResponse(User user) {
         return UserResponse.builder()
-                .id(user.getId().toString())
+                .id(user.getId())
                 .username(user.getUsername())
                 .email(user.getEmail())
                 .fullName(user.getFullName())
